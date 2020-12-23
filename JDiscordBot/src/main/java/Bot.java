b46d304a6f75150e695cea03002abe63bbc304c8
WebDev2080 <52177935+webdev2080@users.noreply.github.com>
2020-12-22 21:30:18 -0800

    Add files via upload

---
+++ JDiscordBot/src/main/java/Bot.java
@@ -0,0 +1,30 @@
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+
+public class Bot {
+
+    public static void main(String[] args) {
+        GatewayDiscordClient client = DiscordClientBuilder.create("NzkxMTU1MDMzMTAxOTU5MTgw.X-LCig.GQWlOICndNoPioNnrIKMLNhVvM0").build().login().block();
+
+        client.getEventDispatcher().on(ReadyEvent.class)
+                .subscribe(event -> {
+                    User self = event.getSelf();
+                    System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
+                });
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Pong!"))
+                .subscribe();
+
+        client.onDisconnect().block();
+    }
+
+}
